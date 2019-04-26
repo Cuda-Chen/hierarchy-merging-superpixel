45b5c03304877b0cfb98a6a76b458569e1e5c30b
Cuda-Chen <clh960524@gmail.com>
2019-04-26 16:48:41 +0800

    add argsparse feature

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from skimage import data, io, segmentation, color
 from skimage.future import graph
 import numpy as np
-
+import argparse
+import os
 
 def _weight_mean_color(graph, src, dst, n):
     """Callback to handle merging nodes by recomputing mean color.
@@ -46,17 +47,28 @@ def merge_mean_color(graph, src, dst):
     graph.node[dst]['mean color'] = (graph.node[dst]['total color'] /
                                      graph.node[dst]['pixel count'])
 
+parser = argparse.ArgumentParser()
+parser.add_argument('input_image', type=str, help='input image path')
+parser.add_argument('num_superpixel', type=int, help='number of segments')
+parser.add_argument('compactness', type=int, help='compactness param of SLIC')
+parser.add_argument('thresh', type=float, help='threshold of combining edge')
+args = parser.parse_args()
 
-img = data.coffee()
-labels = segmentation.slic(img, compactness=30, n_segments=400)
+img = io.imread(args.input_image)
+outputfile = os.path.splitext(args.input_image)[0] \
++ '_' + str(args.num_superpixel) \
++ '_' + str(args.compactness) \
++ '_' + str(args.thresh) + '.bmp'
+
+labels = segmentation.slic(img, n_segments=args.num_superpixel, compactness=args.compactness)
 g = graph.rag_mean_color(img, labels)
 
-labels2 = graph.merge_hierarchical(labels, g, thresh=35, rag_copy=False,
+labels2 = graph.merge_hierarchical(labels, g, thresh=args.thresh, rag_copy=False,
                                    in_place_merge=True,
                                    merge_func=merge_mean_color,
                                    weight_func=_weight_mean_color)
 
 out = color.label2rgb(labels2, img, kind='avg')
-out = segmentation.mark_boundaries(out, labels2, (0, 0, 0))
+#out = segmentation.mark_boundaries(out, labels2, (0, 0, 0))
 io.imshow(out)
 io.show()
